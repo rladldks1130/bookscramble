964c20fb73f38dd389d3e7ca141a043f8e6372cd
rladldks1130 <rladldks1130@gmail.com>
2024-09-12 03:02:45 +0900

    feat: bookscramble

---
+++ bookscramble.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import random
+import time
+
+# 책 목록
+book_list = ["나의 라임오렌지나무", "달러구트 꿈백화점", "허클베리 핀의 모험", "이상한 나라의 앨리스", "미드나잇 라이브러리", "프로젝트 헤일메리", "학교 가기 싫을 땐 겨드랑이에 양파를 끼워 봐", "세상 마지막 기차역", "9번", "10번", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+
+st.title("북스크램블")
+
+# 세션 상태 초기화
+if "start" not in st.session_state:
+	st.session_state.start = False
+	st.session_state.current_index = 0
+	st.session_state.quiz_book = []
+	st.session_state.score = 0
+	st.session_state.key_suffix = 0  # key_suffix를 추가합니다.
+
+# 시작 버튼
+if st.button("start"):
+	st.session_state.start = True
+	st.session_state.quiz_book = random.sample(book_list, 10)
+	st.session_state.current_index = 0
+	st.session_state.score = 0
+	st.session_state.key_suffix = 0  # 게임을 시작할 때 key_suffix를 0으로 초기화합니다.
+	answer = ""
+
+if st.session_state.start:
+	if st.session_state.current_index < 10:
+		# 현재 문제
+		book = st.session_state.quiz_book[st.session_state.current_index].replace(" ", "")
+		scramble = random.sample(book, len(book))
+		quiz = " ".join(scramble)
+		st.write(f"문제 {st.session_state.current_index + 1}: {quiz}")
+
+		# 사용자 입력 받기
+		answer = st.text_input("정답을 입력하세요:", key=f"input{st.session_state.current_index}{st.session_state.key_suffix}")
+
+		# 정답 제출 버튼
+	if st.button("정답 제출", key=f"submit{st.session_state.current_index}"):
+		if answer == st.session_state.quiz_book[st.session_state.current_index]:
+			st.session_state.score += 1
+			st.success(f"정답입니다! {st.session_state.score}/10")
+		else:
+			st.error(f"오답입니다! {st.session_state.score}/10")
+
+		# 다음 문제로 넘어가기 전에 인덱스와 key_suffix를 증가시킨 후 1초 대기
+		if st.session_state.current_index < 9:
+			st.session_state.current_index += 1
+			st.session_state.key_suffix += 1  # key_suffix를 증가시킵니다.
+			time.sleep(1)
+			st.rerun()  # 인덱스를 증가시킨 후 페이지 다시 로드
+		else:
+			st.write(f"{st.session_state.score}/10")
+			if st.session_state.score >= 7:
+				st.balloons()
+				st.success(f"총 {st.session_state.score}문제 맞췄습니다. 통과입니다!")
+			else:
+				st.error(f"총 {st.session_state.score}문제 맞췄습니다. 실패입니다")
+			st.session_state.start = False  # 게임 종료
