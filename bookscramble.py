683d4d9e4f66061ba2fc612882c483b92a38c992
rladldks1130 <rladldks1130@gmail.com>
2024-09-13 12:42:38 +0900

    feat: 글씨 크기 조정

--- bookscramble.py
+++ bookscramble.py
@@ -26,19 +26,19 @@ if "start" not in st.session_state:
 # 시작 버튼
 if st.button("start"):
 	st.session_state.start = True
-	st.session_state.quiz_book = random.sample(book_list, 10)  # 엑셀에서 불러온 책 목록 중 10개 샘플
+	st.session_state.quiz_book = random.sample(book_list, 5)  # 엑셀에서 불러온 책 목록 중 10개 샘플
 	st.session_state.current_index = 0
 	st.session_state.score = 0
 	st.session_state.key_suffix = 0  # 게임을 시작할 때 key_suffix를 0으로 초기화합니다.
 	answer = ""
 
 if st.session_state.start:
-	if st.session_state.current_index < 10:
+	if st.session_state.current_index < 5:
 		# 현재 문제
 		book = st.session_state.quiz_book[st.session_state.current_index].replace(" ", "")
 		scramble = random.sample(book, len(book))
 		quiz = " ".join(scramble)
-		st.markdown(f"<h2 style='font-size: 24px;'>문제 {st.session_state.current_index + 1}: {quiz}</h2>", unsafe_allow_html=True)
+		st.markdown(f"<h2 style='font-size: 35px;'>문제 {st.session_state.current_index + 1}: {quiz}</h2>", unsafe_allow_html=True)
 
 		# 사용자 입력 받기
 		answer = st.text_input("정답을 입력하세요:", placeholder=None, key=f"input{st.session_state.current_index}{st.session_state.key_suffix}")
@@ -48,18 +48,18 @@ if st.session_state.start:
 		answer = answer.replace(" ", "")
 		if answer == book:
 			st.session_state.score += 1
-			st.markdown(f"<h3 style='font-size: 20px;'>정답입니다! {st.session_state.score}/10</h3>", unsafe_allow_html=True)
+			st.markdown(f"<h3 style='font-size: 25px;'>정답입니다! {st.session_state.score}/10</h3>", unsafe_allow_html=True)
 		else:
-			st.markdown(f"<h3 style='font-size: 20px;'>오답입니다! {st.session_state.score}/10</h3>", unsafe_allow_html=True)
+			st.markdown(f"<h3 style='font-size: 25px;'>오답입니다! {st.session_state.score}/10</h3>", unsafe_allow_html=True)
 
 		# 다음 문제로 넘어가기 전에 인덱스와 key_suffix를 증가시킨 후 1초 대기
-		if st.session_state.current_index < 9:
+		if st.session_state.current_index < 4:
 			st.session_state.current_index += 1
 			st.session_state.key_suffix += 1  # key_suffix를 증가시킵니다.
 			time.sleep(1)
 			st.rerun()  # 인덱스를 증가시킨 후 페이지 다시 로드
 		else:
-			if st.session_state.score >= 7:
+			if st.session_state.score >= 3:
 				st.balloons()
 				st.markdown(f"<h3 style='font-size: 24px;'>총 {st.session_state.score}문제 맞췄습니다. 통과입니다!</h3>", unsafe_allow_html=True)
 			else:
